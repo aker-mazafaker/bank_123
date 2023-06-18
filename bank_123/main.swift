68cd515dde0f9d4777930ad56751c98db8306c3c
Aker <@akerke.n@inbox.ru>
2023-06-19 00:44:09 +0600

    functionality

--- bank_123/main.swift
+++ bank_123/main.swift
@@ -1,11 +1,38 @@
-//
-//  main.swift
-//  bank_123
-//
-//  Created by Akerke on 04.06.2023.
-//
 
 import Foundation
 
-print("Hello, World!")
+class Card {
+    var cardNumber: Int
+    var cardHolder: String
+    var cardPassword: Int
+    var isBlocked: Bool
+    
+    init(cardNumber: Int, cardHolder: String, cardPassword: Int, isBlocked: Bool) {
+        self.cardNumber = cardNumber
+        self.cardHolder = cardHolder
+        self.cardPassword = cardPassword
+        self.isBlocked = isBlocked
+    }
+}
 
+class BankAccount{
+    var accountNumber: Int
+    var currencyOfAccount: String
+    var informationAboutMoney: Double
+    
+    init(accountNumber: Int, currencyOfAccount: String, informationAboutMoney: Double) {
+        self.accountNumber = accountNumber
+        self.currencyOfAccount = currencyOfAccount
+        self.informationAboutMoney = informationAboutMoney
+    }
+}
+
+class FunctionalityTemporarily{
+    
+    func openBankAccount ( accountNumber: Int, currencyOfAccount: String, informationAboutMoney: Double ){
+     
+                print("Открыт новый счет с номером \(accountNumber)")
+            }
+            
+    }
+}
